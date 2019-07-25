d2896f22778c2ef5fcafc06a510ab20be641d3ae
jpthor <jpthor@pm.me>
2019-07-25 18:01:06 +1000

    update to env

--- src/env.js
+++ src/env.js
@@ -1,18 +1,15 @@
 const prod_hostnames = [
   "localhost",
 ]
-const stage_hostnames = [
-]
 
-const isMainnet = prod_hostnames.includes(window.location.hostname)
-const isStagenet = stage_hostnames.includes(window.location.hostname)
-const isTestnet = !isMainnet && !isStagenet
+
+const isMainnet = true
+const isTestnet = !isMainnet
 
 const NET = isTestnet ? "testnet" : "mainnet"
 
 export {
   NET,
   isTestnet,
-  isMainnet,
-  isStagenet,
+  isMainnet
 }
