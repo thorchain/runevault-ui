069de53c326a33d84c9c10fd3fe4b30f34f3465c
Ankita Madan <madan.ankita@gmail.com>
2019-12-09 21:47:02 +1100

    fixed api failure exception

--- src/services/runestats.service.js
+++ src/services/runestats.service.js
@@ -11,34 +11,44 @@ import {formatDate} from "../utils/utility";
 import {setLeaderBoardList} from "../actions/leaderboardaction";
 
 export const getLeaderboardlist = () => dispatch => {
-    axios.get("https://frozenbalances.herokuapp.com/frozen/RUNE-B1A")
-        .then((response) => {
-            dispatch(setIsLoading(false));
-            const viewableLeaderBoardList = [];
-            const runeAddressList = response.data;
+    try {
+        axios.get("https://frozenbalances.herokuapp.com/frozen/RUNE-B1A")
+            .then((response) => {
+                dispatch(setIsLoading(false));
+                dispatch(setIsError(false));
+                const viewableLeaderBoardList = [];
+                const runeAddressList = response.data;
 
-            const sortedRuneAddressList = runeAddressList.sort((a, b) => Number(b.frozen) - Number(a.frozen));
+                const sortedRuneAddressList = runeAddressList.sort((a, b) => Number(b.frozen) - Number(a.frozen));
 
-            for (var i = 0; i < sortedRuneAddressList.length; i++) {
-                viewableLeaderBoardList.push({
-                    key: i,
-                    avatar: sortedRuneAddressList[i].address,
-                    address: sortedRuneAddressList[i].address,
-                    staked: (sortedRuneAddressList[i].frozen.toLocaleString()),
-                });
-            }
+                for (var i = 0; i < sortedRuneAddressList.length; i++) {
+                    viewableLeaderBoardList.push({
+                        key: i,
+                        avatar: sortedRuneAddressList[i].address,
+                        address: sortedRuneAddressList[i].address,
+                        staked: (sortedRuneAddressList[i].frozen.toLocaleString()),
+                    });
+                }
 
-            let totalFrozen = sortedRuneAddressList.reduce((acc,address) => { return address.frozen+acc},0);
+                let totalFrozen = sortedRuneAddressList.reduce((acc,address) => { return address.frozen+acc},0);
 
-            dispatch(setSumStake(totalFrozen.toLocaleString()));
-            dispatch(setSakedSupply((totalFrozen/110052528*100).toLocaleString()));
-            dispatch(seTotalStakers(viewableLeaderBoardList.length));
-            dispatch(setLeaderBoardList(viewableLeaderBoardList));
-            dispatch(setDataSource(viewableLeaderBoardList.slice(0, 10)));
-            dispatch(setLastUpdatedDate(formatDate(new Date())));
-            dispatch(setIsError(response.status === 404));
+                dispatch(setSumStake(totalFrozen.toLocaleString()));
+                dispatch(setSakedSupply((totalFrozen/110052528*100).toLocaleString()));
+                dispatch(seTotalStakers(viewableLeaderBoardList.length));
+                dispatch(setLeaderBoardList(viewableLeaderBoardList));
+                dispatch(setDataSource(viewableLeaderBoardList.slice(0, 10)));
+                dispatch(setLastUpdatedDate(formatDate(new Date())));
+                dispatch(setIsError(response.status === 404));
 
-        })
+            })
+            .catch(e => {
+                dispatch(setIsLoading(false));
+                dispatch(setIsError(true));
+            })
+    } catch (e) {
+        dispatch(setIsLoading(false));
+        dispatch(setIsError(true));
+    }
 }
 
 export const saveStakeAddress = (stakeValue) => dispatch => {
